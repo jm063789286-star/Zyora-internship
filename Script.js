76a9bf0f60b1a48332dbc6f9db8cffa381ae788f
jm063789286-star <jm063789286@gmail.com>
2026-07-24 18:59:18 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,28 @@
+const form = document.getElementById("contactForm");
+const result = document.getElementById("result");
 
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (name === "" || email === "" || message === "") {
+        result.style.color = "red";
+        result.innerText = "Please fill all fields.";
+    }
+    else if (!emailPattern.test(email)) {
+        result.style.color = "red";
+        result.innerText = "Enter a valid email.";
+    }
+    else {
+        result.style.color = "green";
+        result.innerText = "Form submitted successfully!";
+    }
+});
 const darkBtn = document.getElementById("darkBtn");
 
 darkBtn.addEventListener("click", function () {
